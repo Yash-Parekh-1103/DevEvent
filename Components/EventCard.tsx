955b758c900bfc88068889f97ca0af5693d85fea
Yash Parekh <parekhyash1103@gmail.comm>
2025-11-29 00:08:36 +0530

    final

--- Components/EventCard.tsx
+++ Components/EventCard.tsx
@@ -16,7 +16,7 @@ interface Props {
 
 const EventCard = ({title , image , slug , location, date, time}: Props) => {
   return (
-    <Link href={`/events/${slug}`} id="event-card">
+    <Link href={`/events/${encodeURIComponent(slug)}`} id="event-card">
 
         <Image src={image} alt={title} width={410} height={300} className='poster' />
 
